744b7bb69556edabd31ce7ee3e24ba06a113dcca
Ariwibowo92 <ariwibowoitt@gmail.com>
2018-11-08 19:14:13 +0700

    add exercise

---
+++ exercise5.js
@@ -0,0 +1,26 @@
+// LOOPING PERTAMA
+// 2 - I love coding
+// 4 - I love coding
+// 6 - I love coding
+// 8 - I love coding
+// 10 - I love coding
+// 12 - I love coding
+// 14 - I love coding
+// 16 - I love coding
+// 18 - I love coding
+// 20 - I love coding
+// LOOPING KEDUA
+// 20 - I will become fullstack developer
+// 18 - I will become fullstack developer                                                                              
+// 16 - I will become fullstack developer
+// 14 - I will become fullstack developer
+// 12 - I will become fullstack developer
+// 10 - I will become fullstack developer
+// 8 - I will become fullstack developer
+// 6 - I will become fullstack developer
+// 4 - I will become fullstack developer
+// 2 - I will become fullstack 
+
+for (var i = 0; i<=20; i+= 2){
+    i === 0 ? console.log('LOOPING PERTAMA'): console.log(i + ' - ' + 'I love coding')
+}
